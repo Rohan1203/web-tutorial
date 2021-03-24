8aba33880488fa25914dcdbcbfad5e74c684270a
Rohan Kumar Das <rohan.das1203@gmail.com>
2021-03-24 12:21:48 +0530

    Third Commit

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'))
+    res.sendFile(path.join(__dirname + '/index.html'))
 });
 
 app.get('/signup', function(req, res) {
     res.sendFile(path.join(__dirname + '/views/signup.html'));
-  });
+});
 
 
 
-
-const port = 9500;
-const ip = 'localhost' 
-app.listen(port, ip, ()=>{
+const port = 9600;
+const ip = 'localhost'
+app.listen(port, ip, () => {
     console.log('Server stared at port :', port);
 })
